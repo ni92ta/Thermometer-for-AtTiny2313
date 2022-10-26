4bea9c5c1a0486eecb173b51083ca7a7bf8aec3e
Nikita Romanovich <nikitarom@hotmail.com>
2022-10-26 20:16:36 +0300

    Create wiree.d

---
+++ Debug/wiree.d
@@ -0,0 +1,56 @@
+wiree.d wiree.o: .././wiree.c .././wiree.h .././main.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\xc.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\cci.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\io.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\inttypes.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h \
+ E:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/atmel/ATtiny_DFP/1.8.332/xc8/avr/include/avr/iotn2313a.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\common.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\version.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\lock.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\interrupt.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay_basic.h \
+ d:\program\ files\microchip\xc8\v2.31\avr\avr\include\math.h
+
+.././wiree.h:
+
+.././main.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\xc.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\cci.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\io.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\inttypes.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h:
+
+E:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/atmel/ATtiny_DFP/1.8.332/xc8/avr/include/avr/iotn2313a.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\common.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\version.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\lock.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\interrupt.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\util\delay_basic.h:
+
+d:\program\ files\microchip\xc8\v2.31\avr\avr\include\math.h:
